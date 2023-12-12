a289077f9874dca30f07fa4bec431136cf02c98e
dheat23 <domsredirects@gmail.com>
2023-12-12 15:46:49 +0000

    add form validation and err/success messages for posting comments

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -6,31 +6,44 @@ const CommentAdder = ({article_id, setComments}) => {
   const { user } = useContext(UserContext);
   const [input, setInput] = useState("");
   const [posting, setPosting] = useState(false);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   function handleInput(e) {
     setInput(e.target.value)
   }
   function handleSubmit(e) {
     e.preventDefault();
-    setPosting(true)
-    postComment(article_id, user, input)
-    .then((comment) => {
+    if(input.length !== 0) {
+      setError(false)
+      setPosting(true)
+      postComment(article_id, user, input)
+      .then((comment) => {
         setPosting(false)
+        setSuccess(true)
+        setTimeout(()=>{
+          setSuccess(false)
+        }, 3000)
         setComments((currComments) => {
-            return [comment, ...currComments]
+          return [comment, ...currComments]
         })
-    })
-    setInput("")
+      })
+      .catch((err) => {
+        setError(true)
+        setPosting(false)
+      })
+      setInput("")
+    }
   }
-  if(posting) {
-    return <p>Posting comment..</p>
-  }
+ 
   return (
     <div id="comment-adder">
       <h3>Add a comment</h3>
       <form id="comment-adder-form" onSubmit={handleSubmit}>
         <textarea onChange={handleInput} id="comment-adder-text" cols={40} rows={5} name="comment-text" placeholder="write your comment here.." value={input}/>
-        <button id="comment-adder-btn">Submit</button>
+        {posting ? <p>Posting comment..</p> : <button id="comment-adder-btn">Submit</button>}
+        {error === true && <p className="error">Error posting comment, try again later</p>}
+        {success === true && <p className="success">Success!</p>}
       </form>
     </div>
   );
