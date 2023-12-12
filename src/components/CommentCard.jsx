5b145d7cb05da7e4a81341ec1c0fdd6e01824ff4
dheat23 <domsredirects@gmail.com>
2023-12-12 15:26:14 +0000

    wip delete comments

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,10 +1,16 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
 const CommentCard = ({ comment }) => {
+  const {user} = useContext(UserContext);
+  const [deleted, setDeleted] = useState(false);
+  
   return (
     <div className="comment-card">
       <p className="comment-author">{comment.author}</p>
       <p className="comment-date">{comment.created_at}</p>
       <p className="comment-body">{comment.body}</p>
       <p className="comment-votes">{comment.votes} votes</p>
+      {user === comment.author && <button className="delete-comment-btn">Delete</button>}
     </div>
   );
 };
