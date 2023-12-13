645e78ad5cd602bc19b764af0c3c11d61570a608
dheat23 <domsredirects@gmail.com>
2023-12-13 13:37:12 +0000

    add styling to display selected sort and order selections

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import {getAllArticles} from "../utils/utils";
+import { getAllArticles } from "../utils/utils";
 import ArticleCard from "./ArticleCard";
 import { ColorRing } from "react-loader-spinner";
 import { useSearchParams } from "react-router-dom";
@@ -12,39 +12,45 @@ const ArticlesList = () => {
   const selectedTopic = searchParams.get("topic");
   const selectedSort = searchParams.get("sort");
   const selectedOrder = searchParams.get("order");
-  console.log(searchParams)
   useEffect(() => {
-    getAllArticles(selectedTopic, selectedSort, selectedOrder).then((articles) => {
-      setArticles(articles);
-      setLoading(false)
-    });
+    setLoading(true);
+    getAllArticles(selectedTopic, selectedSort, selectedOrder).then(
+      (articles) => {
+        setArticles(articles);
+        setLoading(false);
+      }
+    );
   }, [searchParams]);
 
-  if(loading) {
-    return <ColorRing
-    visible={true}
-    height="80"
-    width="80"
-    ariaLabel="color-ring-loading"
-    wrapperStyle={{"textAlign": "center"}}
-    wrapperClass="blocks-wrapper"
-    colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-  />
-  }
   return (
     <section id="articles-list">
-      <h2>{selectedTopic ? "" : "All "}Articles{selectedTopic ? ` about ${selectedTopic}` : ""}</h2>
-      <TopicFilter selectedTopic={selectedTopic}/>
-      <SortBar setSearchParams={setSearchParams}/>
-      <ul>
-        {articles.map((article) => {
-          return (
-            <li key={article.article_id}>
-              <ArticleCard article={article} />
-            </li>
-          );
-        })}
-      </ul>
+      <h2>
+        {selectedTopic ? "" : "All "}Articles
+        {selectedTopic ? ` about ${selectedTopic}` : ""}
+      </h2>
+      <TopicFilter selectedTopic={selectedTopic} />
+      <SortBar searchParams={searchParams} setSearchParams={setSearchParams} />
+      {loading ? (
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="color-ring-loading"
+          wrapperStyle={{ textAlign: "center" }}
+          wrapperClass="blocks-wrapper"
+          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+        />
+      ) : (
+        <ul>
+          {articles.map((article) => {
+            return (
+              <li key={article.article_id}>
+                <ArticleCard article={article} />
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </section>
   );
 };
--- src/components/SortBar.jsx
+++ src/components/SortBar.jsx
@@ -1,38 +1,71 @@
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
-const SortBar = ({setSearchParams}) => {
-    function handleSortClick(sort) {
-        setSearchParams((currSearchParams)=>{
-            let newSearchParams = new URLSearchParams(currSearchParams);
+const SortBar = ({ searchParams, setSearchParams }) => {
+    const sortParam = searchParams.get("sort");
+    const orderParam = searchParams.get("order");
+  function handleSortClick(sort) {
+    setSearchParams((currSearchParams) => {
+      let newSearchParams = new URLSearchParams(currSearchParams);
 
-            newSearchParams.set('sort', sort);
+      newSearchParams.set("sort", sort);
 
-          return newSearchParams;
-        })
-    }
-    function handleOrderClick(order) {
-        setSearchParams((currSearchParams)=>{
-            let newSearchParams = new URLSearchParams(currSearchParams);
+      return newSearchParams;
+    });
+  }
+  function handleOrderClick(order) {
+    setSearchParams((currSearchParams) => {
+      let newSearchParams = new URLSearchParams(currSearchParams);
 
-            newSearchParams.set('order', order)
+      newSearchParams.set("order", order);
 
-            return newSearchParams
-        })
-    }
-    return (<div id="sort-bar">
-        <h3>Sort Bar</h3>
-        <ul>
-            <button onClick={()=>{handleSortClick("created_at")}}>Date</button>
-            <button onClick={()=>{handleSortClick("comment_count")}}>Comment Count</button>
-            <button onClick={()=>{handleSortClick("votes")}}>Votes</button>
-            <button onClick={()=>{
-                handleOrderClick("asc")
-            }}>Ascending</button>
-            <button onClick={()=>{
-                handleOrderClick("desc")
-            }}>Descending</button>
-        </ul>
-    </div>)
-}
+      return newSearchParams;
+    });
+  }
+  return (
+    <div id="sort-bar">
+      <h3>Sort By:</h3>
+      <ul id="sort-by-list">
+        <button className={`sort-button ${sortParam === "created_at" || !sortParam ? "selected-sort-button" : ""}`}
+          onClick={() => {
+            handleSortClick("created_at");
+          }}
+        >
+          Date
+        </button>
+        <button className={`sort-button ${sortParam === "comment_count" ? "selected-sort-button" : ""}`}
+          onClick={() => {
+            handleSortClick("comment_count");
+          }}
+        >
+          Comment Count
+        </button>
+        <button className={`sort-button ${sortParam === "votes" ? "selected-sort-button" : ""}`}
+          onClick={() => {
+            handleSortClick("votes");
+          }}
+        >
+          Votes
+        </button>
+      </ul>
+      <h3>Order By:</h3>
+      <ul id="order-by-list">
+      <button className={`order-button ${orderParam === "asc" ? "selected-sort-button" : ""}`}
+          onClick={() => {
+            handleOrderClick("asc");
+          }}
+        >
+          Ascending
+        </button>
+        <button className={`order-button ${orderParam === "desc" || !orderParam ? "selected-sort-button" : ""}`}
+          onClick={() => {
+            handleOrderClick("desc");
+          }}
+        >
+          Descending
+        </button>
+      </ul>
+    </div>
+  );
+};
 
-export default SortBar
+export default SortBar;
