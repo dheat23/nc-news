beafa0143fd1b9e292d9b9ac1428dc9f5574cfed
dheat23 <domsredirects@gmail.com>
2023-12-15 10:06:31 +0000

    disable random button before articles fetched

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,16 @@ import ArticleCard from "./ArticleCard";
 const Home = () => {
     const [articles, setArticles] = useState([{}]);
     const [randomNum, setRandomNum] = useState(null);
+    const [disable, setDisable] = useState(true);
+    const [error, setError] = useState(null);
     useEffect(()=>{
         getAllArticles()
         .then((articles)=>{
             setArticles(articles)
+            setDisable(false)
+        })
+        .catch((err) => {
+            setError(err.data.msg)
         })
     }, [])
     function handleRandomClick() {
@@ -17,11 +23,14 @@ const Home = () => {
         
         
     };
+    if(error) {
+        return <p className="error">{error}</p>
+    }
     return (
         <section id="home">
             <h2>Welcome to Northcoders News!</h2>
             <p>Check out a random article below, or go to <Link to='/articles'>Articles</Link> to view the full list</p>
-            <button onClick={handleRandomClick}>View Random Article</button>
+            <button className={disable ? "disabled-btn" : ""} onClick={handleRandomClick}>View Random Article</button>
             {randomNum === null ? <div id="mystery-random-article"></div> : <ArticleCard article={articles[randomNum]} />}
         </section>
     )
